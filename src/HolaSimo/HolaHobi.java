528346cdd7b67f9c604bc3082457ad8472c3542e
ssimofatine <simohaji147@gmail.com>
2025-02-21 13:46:55 +0100

    Comienzo de proyecto

---
+++ src/HolaSimo/HolaHobi.java
@@ -0,0 +1,15 @@
+package HolaSimo;
+
+public class HolaHobi {
+	
+	public static void main(String[] args) {
+		int a , b, c;
+		a = 5;
+		b = 6;
+		c = 9;
+		int k = a + b + c;
+		System.out.println("Hola Mundo = " + k);
+		
+	}
+	
+}
