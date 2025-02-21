ae53f9ee14bffe95c46489798531d60657fb513f
ssimofatine <simohaji147@gmail.com>
2025-02-21 15:08:49 +0100

    simohaji

---
+++ src/HolaSimo/holahajiha.java
@@ -0,0 +1,13 @@
+package HolaSimo;
+
+public class holahajiha {
+	public static void main(String[] args) {
+		int a , b, c;
+		a = 5;
+		b = 6;
+		c = 9;
+		int k = a + b + c;
+		System.out.println("Hola Mundo = " + k);
+		
+	}
+}
